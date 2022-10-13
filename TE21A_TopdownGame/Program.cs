e2afa0ffce59fe6c2d9b2e18a9cdf1ef3e99876b
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-10-13 15:32:38 +0200

    Initial commit

---
+++ TE21A_TopdownGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(1024, 768, "Topdown game");
+Raylib.SetTargetFPS(60);
+
+Rectangle character = new Rectangle(0, 60, 50, 50);
+float speed = 4.5f;
+
+//                       r   g    b    a
+Color myColor = new Color(0, 200, 30, 255);
+
+while (Raylib.WindowShouldClose() == false)
+{
+  if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+  {
+    character.x += speed;
+  }
+
+  Raylib.BeginDrawing();
+
+  Raylib.ClearBackground(Color.WHITE);
+
+  Raylib.DrawRectangleRec(character, myColor);
+
+  // Raylib.DrawRectangle(0, 60, 50, 50, myColor);
+  // Raylib.DrawRectangle(0, 70, 50, 50, Color.RED);
+
+  Raylib.EndDrawing();
+}
